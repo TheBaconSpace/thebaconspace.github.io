6511d5bfdcdd951ed47c45a41b793467ee400fe8
Bacon_Space <thespaceguild@gmail.com>
2023-10-06 18:53:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((data) => {
             const socialLinksSection = document.getElementById("social-links");
 
-            // Generate buttons for each social link
+            // Generate button-links for each social link
             data.socialLinks.forEach((link) => {
                 const socialButton = document.createElement("a");
                 socialButton.href = link.url;
                 socialButton.target = "_blank";
                 socialButton.className = "btn btn-primary mr-2 mb-2";
-                socialButton.innerHTML = `<i class="${link.icon}"></i> ${link.name}`;
+                socialButton.innerHTML = `${link.name} <i class="${link.icon}"></i>`;
 
                 socialLinksSection.appendChild(socialButton);
             });
