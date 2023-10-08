f09989233474765926d67310d71fa85dfa4bff81
Bacon_Space <thespaceguild@gmail.com>
2023-10-08 18:25:18 -0400

    Create script.js

---
+++ champs/script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('champs.json')
+        .then(response => response.json())
+        .then(data => {
+            const championsContainer = document.getElementById('champions');
+            
+            for (const category in data) {
+                const champion = data[category];
+                const championList = document.createElement('ul');
+                championList.className = 'list-group mt-3';
+                
+                if (typeof champion === 'object') {
+                    for (const name in champion) {
+                        const title = champion[name];
+                        const listItem = document.createElement('li');
+                        listItem.className = 'list-group-item';
+                        listItem.textContent = `${name}: ${title}`;
+                        championList.appendChild(listItem);
+                    }
+                } else {
+                    const listItem = document.createElement('li');
+                    listItem.className = 'list-group-item';
+                    listItem.textContent = `${category}: ${champion}`;
+                    championList.appendChild(listItem);
+                }
+                
+                championsContainer.appendChild(championList);
+            }
+        })
+        .catch(error => console.error(error));
+});
