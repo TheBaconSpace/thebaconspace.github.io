b4c765ef8bd63fc17aa19309de8071e593240b9c
Bacon_Space <thespaceguild@gmail.com>
2023-10-14 23:17:33 -0400

    Update script.js

--- champs/script.js
+++ champs/script.js
@@ -3,30 +3,29 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => response.json())
         .then(data => {
             const championsContainer = document.getElementById('champions');
-            
+
             for (const category in data) {
                 const champion = data[category];
                 const championList = document.createElement('ul');
                 championList.className = 'list-group mt-3';
-                
+
                 if (typeof champion === 'object') {
                     for (const name in champion) {
                         const title = champion[name];
                         const listItem = document.createElement('li');
                         listItem.className = 'list-group-item';
-                        listItem.textContent = `${name}: ${title}`;
+                        listItem.innerHTML = `<span class="badge text-bg-primary">BSW</span> ${name}: ${title}`;
                         championList.appendChild(listItem);
                     }
                 } else {
                     const listItem = document.createElement('li');
                     listItem.className = 'list-group-item';
-                    listItem.textContent = `${category}: ${champion}`;
+                    listItem.innerHTML = `<span class="badge text-bg-primary">BSW</span> ${category}: ${champion}`;
                     championList.appendChild(listItem);
                 }
-                
+
                 championsContainer.appendChild(championList);
             }
         })
         .catch(error => console.error(error));
 });
-
